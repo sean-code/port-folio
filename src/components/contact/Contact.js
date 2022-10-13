571760f354320f401e6bafa379bd6a459c2aa721
sean-code <john.nganga@student.moringaschool.com>
2022-10-13 09:15:54 +0300

    Edit the Contact me Cards and form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,57 +1,124 @@
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import './Contact.css';
 
 function Contact(){
     return(
-        <div className="container">
-            <h2>Contact Me</h2>
-            <div className="row100">
-                <div className="col">
-                    <div className="inputBox">
-                        <input type="text" name="" required="required" />
-                        <span className="text">First Name</span>
-                        <span className="line"></span>
+        <>
+            <div className="container">
+                <h2>Contact Me</h2>
+                <div className="row100">
+                    <div className="col">
+                        <div className="inputBox">
+                            <input type="text" placeholder= 'First name' name="" required="required" />
+                            <span className="line"></span>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="inputBox">
+                            <input type="text" placeholder= 'Last Name' name="" required="required" />
+                            <span className="line"></span>
+                        </div>
                     </div>
                 </div>
-                <div className="col">
-                    <div className="inputBox">
-                        <input type="text" name="" required="required" />
-                        <span className="text">Last Name</span>
-                        <span className="line"></span>
+                <div className="row100">
+                    <div className="col">
+                        <div className="inputBox">
+                            <input type="text" placeholder= 'Email' name="" required="required" />
+                            <span className="line"></span>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="inputBox">
+                            <input type="number" placeholder='Mobile Number' name="" required="required" />
+                            <span className="line"></span>
+                        </div>
+                    </div>
+                </div>
+                <div className="row100">
+                    <div className="col">
+                        <div className="inputBox textarea">
+                            <textarea required="required" placeholder='Type your message Here...'></textarea>
+                            <span className="text"></span>
+                            <span className="line"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row100">
+                    <div class="col">
+                        <input type="submit" value="Send" />
                     </div>
                 </div>
             </div>
-            <div className="row100">
-                <div className="col">
-                    <div className="inputBox">
-                        <input type="text" name="" required="required" />
-                        <span className="text">Email</span>
-                        <span className="line"></span>
+            <div className='container2'>
+                <div className="card">
+                    <a href='https://www.twitter.com/nganga_sea' target='_blank'>
+                        <h3 className="title">Twitter</h3>
+                    </a>
+                    <div className="bar">
+                        <div className="emptybar"></div>
+                        <div className="filledbar"></div>
+                    </div>
+                    <div className="circle">
+                        <a href='https://www.twitter.com/nganga_sea' target='_blank'>
+                            @nganga_sea
+                        </a>
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+                        <circle className="stroke" cx="60" cy="60" r="50" />
+                        </svg>
                     </div>
                 </div>
-                <div className="col">
-                    <div className="inputBox">
-                        <input type="number" name="" required="required" />
-                        <span className="text">Mobile</span>
-                        <span className="line"></span>
+                <div className="card">
+                    <a href='https://github.com/sean-code' target='_blank'>
+                        <h3 className="title">GitHub</h3>
+                    </a>
+                    <div className="bar">
+                        <div className="emptybar"></div>
+                        <div className="filledbar"></div>
+                    </div>
+                    <div className="circle">
+                        <a href='https://github.com/sean-code' target='_blank'>
+                            @Sean-Code
+                        </a>
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+                            <circle className="stroke" cx="60" cy="60" r="50"/>
+                        </svg>
+                    </div>
+                </div>
+                <div className="card">
+                    <a href='https://www.instagram.com/its_ngangasean' target='_blank'>
+                        <h3 className="title">Instagram</h3>
+                    </a>
+                    <div className="bar">
+                        <div className="emptybar"></div>
+                        <div className="filledbar"></div>
+                    </div>
+                    <div className="circle">
+                        <a href='https://www.instagram.com/its_ngangasean' target='_blank'>
+                            @its_ngangasean
+                        </a>
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+                        <circle className="stroke" cx="60" cy="60" r="50"/>
+                    </svg>
+                    </div>
+                </div>
+                <div className="card">
+                    <a href="mailto:email@example.com" target='_blank'>
+                        <h3 className="title">Email</h3>
+                    </a>
+                    <div className="bar">
+                        <div className="emptybar"></div>
+                        <div className="filledbar"></div>
+                    </div>
+                    <div className="circle">
+                        <a href="mailto:email@example.com" target='_blank'>
+                            ngangasean@yahoo.com
+                        </a>
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+                            <circle className="stroke" cx="60" cy="60" r="50"/>
+                        </svg>
                     </div>
                 </div>
             </div>
-            <div className="row100">
-                <div className="col">
-                    <div className="inputBox textarea">
-                        <textarea required="required" placeholder='Type your message Here...'></textarea>
-                        <span className="text"></span>
-                        <span className="line"></span>
-                    </div>
-                </div>
-            </div>
-            <div class="row100">
-                <div class="col">
-                    <input type="submit" value="Send" />
-                </div>
-            </div>
-        </div>
+        </> 
         )
     }
 
--- src/components/landing/Home.js
+++ src/components/landing/Home.js
@@ -1,5 +1,10 @@
 import './Home.css';
 import display from './profile.jpeg';
+import mail from './mail.svg';
+import linkedin from './linkedin.svg';
+import instagram from './instagram.svg';
+import twitter from './twitter.svg';
+
 
 function Home(){
     return(
@@ -13,6 +18,16 @@ function Home(){
                     Here goes my official portfolio page.<br />
                     I'm a tech-enthusiast who curates experiences with technology
                 </h3>
+                <a href='https://www.linkedin.com/in/john-sean-kangethe/'>
+                    <img src={linkedin} />
+                </a>
+                <a href='https://www.twitter.com/nganga_sea'>
+                    <img src={twitter} />     
+                </a>
+                <a href='https://www.instagram.com/its_ngangasean'>
+                    <img src={instagram} />
+                </a>
+                <img src ={mail} />
             </div>
         </div>
     )
