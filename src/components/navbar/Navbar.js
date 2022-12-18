06462c4e2e2917e6a65f130f3f8fa1f8bc7ef41a
sean-code <john.nganga@student.moringaschool.com>
2022-12-18 16:57:22 +0300

    Style the Bavbar and add a navlink to the logo

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,11 +6,11 @@ function Navbar(){
     return(
         <nav className="header">
             <h1 className="logo">
-                <a href="#">John S. Nganga</a>
+                <a href="/home">John S. Nganga</a>
             </h1>
             <ul className="main-nav">
                 <li className="nav-list-item">
-                    <NavLink to='/' className="nav-link">Home</NavLink>
+                    <NavLink to='/home' className="nav-link">Home</NavLink>
                 </li>
                 <li className="nav-list-item">
                     <NavLink to='/about' className="nav-link">About Me</NavLink>
@@ -22,7 +22,7 @@ function Navbar(){
                     <NavLink to='/contact' className="nav-link">Contact</NavLink>
                 </li>
                 <li className="nav-list-item">
-                    <a href='https://medium.com/@nganga_sean/seans-friend-s-future-past-bfe121d00d53' target='_blank'>
+                    <a href='https://medium.com/@nganga_sean/seans-friend-s-future-past-bfe121d00d53' target={'_blank'}>
                         <button className="btn-1">Blog</button>
                     </a>
                 </li>
