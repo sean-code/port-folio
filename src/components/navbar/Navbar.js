adfbfa91233e19b147cbe5f04ac0a75ddd16dd5b
sean-code <john.nganga@student.moringaschool.com>
2022-12-18 12:14:15 +0300

    Restructure the build of the Navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,28 +4,29 @@ import './Navbar.css'
 
 function Navbar(){
     return(
-        <nav className="main-navigation">
-            <div className="navbar">
-                <ul className="nav-list">
-                    <li className="nav-list-item">
-                        <NavLink to='/' className="nav-link">Home</NavLink>
-                    </li>
-                    <li className="nav-list-item">
-                        <NavLink to='/about' className="nav-link">About Me</NavLink>
-                    </li>
-                    <li className="nav-list-item">
-                        <NavLink to='/service' className="nav-link">Services</NavLink>
-                    </li>
-                    <li className="nav-list-item">
-                        <NavLink to='/contact' className="nav-link">Contact</NavLink>
-                    </li>
-                    <li className="nav-list-item">
-                        <a href='https://medium.com/@nganga_sean/seans-friend-s-future-past-bfe121d00d53' target='_blank'>
+        <nav className="header">
+            <h1 className="logo">
+                <a href="#">Flexbox</a>
+            </h1>
+            <ul className="main-nav">
+                <li className="nav-list-item">
+                    <NavLink to='/' className="nav-link">Home</NavLink>
+                </li>
+                <li className="nav-list-item">
+                    <NavLink to='/about' className="nav-link">About Me</NavLink>
+                </li>
+                <li className="nav-list-item">
+                    <NavLink to='/service' className="nav-link">Services</NavLink>
+                </li>
+                <li className="nav-list-item">
+                    <NavLink to='/contact' className="nav-link">Contact</NavLink>
+                </li>
+                <li className="nav-list-item">
+                    <a href='https://medium.com/@nganga_sean/seans-friend-s-future-past-bfe121d00d53' target='_blank'>
                         <button className="btn-1">Blog</button>
-                        </a>
-                    </li>
-                </ul>
-            </div>
+                    </a>
+                </li>
+            </ul>
         </nav>
     )
 }
