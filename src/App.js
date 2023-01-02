57ca85843620069b42a77715ca26a960b4f978c0
sean-code <john.nganga@student.moringaschool.com>
2023-01-02 15:07:28 +0300

    Add HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/landing/Home';
 import Navbar from './components/navbar/Navbar';
 import About from './components/about/About';
