7b3e8b93a9a9aebe7dee9f523df843dffa2c7913
sean-code <john.nganga@student.moringaschool.com>
2023-01-02 13:46:55 +0300

    Add backend for handling submission

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,55 +1,64 @@
 import './Contact.css';
 
 function Contact(){
+
+
     return(
         <>
             <div className="container">
                 <h2><u>Co</u>ntact Me</h2>
                 <div className="row100">
                     <div className="col">
-                        <div className="inputBox">
-                            <input type="text" placeholder= 'First name' name="" required="required" />
-                            <span className="line"></span>
+
+                {/* CREATE FORM */}
+                    <form action='https://app.headlessforms.cloud/api/v1/form-submission/JgbXegaT6O' method='POST'>
+                        <div className='col'>
+                            <div className='inputBox'>
+                                <input type="text" placeholder= 'First Name' name=" FirstName" required="required" />
+                                <span className="line"></span>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col">
-                        <div className="inputBox">
-                            <input type="text" placeholder= 'Last Name' name="" required="required" />
-                            <span className="line"></span>
+                        <div className='col'>
+                        <div className='inputBox'>
+                                <input type="text" placeholder= 'Last Name' name="LastName" required="required" />
+                                <span className="line"></span>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="row100">
-                    <div className="col">
-                        <div className="inputBox">
-                            <input type="text" placeholder= 'Email' name="" required="required" />
-                            <span className="line"></span>
+                        <div className='col'>
+                        <div className='inputBox'>
+                                <input type="number" placeholder= 'Phone Number' name="PhoneNumber" required="required" />
+                                <span className="line"></span>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col">
-                        <div className="inputBox">
-                            <input type="number" placeholder='Mobile Number' name="" required="required" />
-                            <span className="line"></span>
+                        <div className='row100'>
+                            <div className='col'>
+                                <div className='inputBox'>
+                                    <input type="text" placeholder= 'Email' name="Email" required="required" />
+                                    <span className="line"></span>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="row100">
-                    <div className="col">
                         <div className="inputBox textarea">
-                            <textarea required="required" placeholder='Type your message Here...'></textarea>
+                            <textarea name="Message" required="required" placeholder='Type your message Here...'></textarea>
                             <span className="text"></span>
                             <span className="line"></span>
                         </div>
-                    </div>
+
+                        {/* Form Submit Button */}
+                        <div className='row100'>
+                            <div className='col'>
+                                <button type="submit" value="Send">
+                                    Submit
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                <div class="row100">
-                    <div class="col">
-                        <a href="https://t.me/sean_code" target="_blank">
-                            <input type="submit" value="Send" />
-                        </a>
-                    </div>
                 </div>
             </div>
+                        {/* End of Form */}
+
+                    {/* Contact me stickers */}
             <div className='container2'>
                 <div className="card">
                     <a href='https://www.twitter.com/nganga_sea' target='_blank'>
