9660c2f3eb6f3e4f39d7f442c48ba9bcfee24ce7
sean-code <john.nganga@student.moringaschool.com>
2023-01-02 14:59:20 +0300

    Add Star dust

--- src/components/landing/Home.js
+++ src/components/landing/Home.js
@@ -8,34 +8,46 @@ import twitter from './twitter.svg';
 
 function Home(){
     return(
-        <div className='Home'>
-            <div className='home-image'>
-                <img src={display} alt='display-photo'/>
+        <>
+            {/* dust particles */}
+
+            <div>
+                <div className="starsec"></div>
+                <div className="starthird"></div>
+                <div className="starfourth"></div>
+                <div className="starfifth"></div>
             </div>
-            <div className='Details'>
-                <h1>Howdy, I'm <span>John Nganga</span></h1>
-                <h3>
-                    I'm a tech-enthusiast who curates experience with technology
-                </h3>
-                <a href='https://www.linkedin.com/in/john-sean-kangethe/' target={'_blank'}>
-                    <img src={linkedin} alt='img'/>
-                </a>
-                <a href='https://www.twitter.com/nganga_sea' target={"_blank"}>
-                    <img src={twitter} alt='img' />     
-                </a>
-                <a href='https://www.instagram.com/its_ngangasean' target={"_blank"}>
-                    <img src={instagram} alt='img' />
-                </a>
-                <a href="https://t.me/sean_code" target={"_blank"}>
-                    <img src ={mail} alt='img' />
-                </a>
+
+            {/* Rest of Page */}
+            <div className='Home'>
+                <div className='home-image'>
+                    <img src={display} alt='display-photo'/>
+                </div>
+                <div className='Details'>
+                    <h1>Howdy, I'm <span>John Nganga</span></h1>
+                    <h3>
+                        I'm a tech-enthusiast who curates experience with technology
+                    </h3>
+                    <a href='https://www.linkedin.com/in/john-sean-kangethe/' target={'_blank'}>
+                        <img src={linkedin} alt='img'/>
+                    </a>
+                    <a href='https://www.twitter.com/nganga_sea' target={"_blank"}>
+                        <img src={twitter} alt='img' />     
+                    </a>
+                    <a href='https://www.instagram.com/its_ngangasean' target={"_blank"}>
+                        <img src={instagram} alt='img' />
+                    </a>
+                    <a href="https://t.me/sean_code" target={"_blank"}>
+                        <img src ={mail} alt='img' />
+                    </a>
+                </div>
+                <button className='btn-2'>
+                    <a href='https://docs.google.com/document/d/1LrfTMyCT-S8DgPljuiC2jGwIC10wyReFbGF-a8QlWRU/edit?usp=sharing' target='_blank'>
+                        Download CV
+                    </a>
+                </button>
             </div>
-            <button className='btn-2'>
-                <a href='https://docs.google.com/document/d/1LrfTMyCT-S8DgPljuiC2jGwIC10wyReFbGF-a8QlWRU/edit?usp=sharing' target='_blank'>
-                    Download CV
-                </a>
-            </button>
-        </div>
+        </>
     )
 }
 
