46fbbd1bd77c51900821591841b1fb47fb54b8d5
sean-code <john.nganga@student.moringaschool.com>
2023-01-05 19:43:48 +0300

    Finish up on cards

--- src/components/project/Projects.js
+++ src/components/project/Projects.js
@@ -12,7 +12,7 @@ function Projects(){
               <div className="portfolio-container">
                 <div className="card">
                   <div className="card-preview">
-                    <img src="https://i.ibb.co/R713vWz/Screenshot-from-2022-12-18-15-21-20.png" alt="placeholder" className="card-preview__img" />
+                    <img src="https://i.pinimg.com/564x/7e/c1/11/7ec111864f7539dce5362ccf235b61a4.jpg" alt="placeholder" className="card-preview__img" />
                   </div>
                   <div className="card-content">
                     <h4 className="card-content__title">Music Life+</h4>
@@ -28,7 +28,7 @@ function Projects(){
                 </div>
                 <div className="card">
                   <div className="card-preview">
-                    <img src="https://i.ibb.co/N2QrCvc/Screenshot-from-2022-12-18-15-43-53.png" alt="placeholder" className="card-preview__img" />
+                    <img src="https://cdn.dribbble.com/users/1508697/screenshots/5982299/media/5f4678715c4592134ed1dafa26379695.png?compress=1&resize=400x300" alt="placeholder" className="card-preview__img" />
                     </div>
                   <div className="card-content">
                     <h4 className="card-content__title">Marvel Stars UI</h4>
@@ -44,24 +44,7 @@ function Projects(){
                 </div>
                 <div className="card">
                   <div className="card-preview">
-                    <img src="https://previews.123rf.com/images/ylivdesign/ylivdesign1304/ylivdesign130400043/19244692-green-movie-logo-on-a-white-background.jpg" alt="placeholder" className="card-preview__img" /></div>
-                  <div className="card-content">
-                    <h4 className="card-content__title">Movie Search App</h4>
-                    <small>Built using React js, react node, OMDB, CSS, Bootstrap</small>
-                    <p className="card-content__text">A website application that provides a user with the ability to search all categories of movies. 
-                  The idea is to provide entertainment to the user. Furthermore, a user is able to compile a list of favorite movies. All of this among other amazing UI features are meant for entertainment. 
-                  The Project was done for the purpose of learning and to grasp hands-on skills in website application development using React JS, and as well interaction with a .</p>
-                    <p><a href="https://sean-code.github.io/Movie-Search/#/" target="_blank">
-                      View Online
-                      </a></p>
-                    <p><a href="https://github.com/sean-code/Movie-Search" target="_blank">
-                      View Repo
-                      </a></p>
-                  </div>
-                </div>
-                <div className="card">
-                  <div className="card-preview">
-                    <img src="https://sean-code.github.io/profile-card/img/13A0A243-165A-43B5-8741-D2C1FD3DA9CA.jpg" alt="placeholder" className="card-preview__img" /></div>
+                    <img src="https://i.pinimg.com/236x/7a/e9/00/7ae9008fb149ab8b5e4404aafc81b1b6.jpg" alt="placeholder" className="card-preview__img" /></div>
                   <div className="card-content">
                     <h4 className="card-content__title"> Display Profile Card</h4>
                     <small>Built using HTML, CSS, SCSS, SASS</small>
@@ -77,12 +60,28 @@ function Projects(){
                 </div>
                 <div className="card">
                   <div className="card-preview">
-                    <img src="https://github.com/sean-code/tic-tac/raw/main/screenshot.gif" alt="placeholder" className="card-preview__img" /></div>
+                    <img src="https://previews.123rf.com/images/ylivdesign/ylivdesign1304/ylivdesign130400043/19244692-green-movie-logo-on-a-white-background.jpg" alt="placeholder" className="card-preview__img" /></div>
+                  <div className="card-content">
+                    <h4 className="card-content__title">Movie Search App</h4>
+                    <small>Built using React js, react node, OMDB, CSS, Bootstrap</small>
+                    <p className="card-content__text">A website application that provides a user with the ability to search all categories of movies. 
+                  The idea is to provide entertainment to the user.</p>
+                    <p><a href="https://sean-code.github.io/Movie-Search/#/" target="_blank">
+                      View Online
+                      </a></p>
+                    <p><a href="https://github.com/sean-code/Movie-Search" target="_blank">
+                      View Repo
+                      </a></p>
+                  </div>
+                </div>
+                <div className="card">
+                  <div className="card-preview">
+                    <img src="https://previews.123rf.com/images/logomimi/logomimi2204/logomimi220400150/184547875-simple-tic-tac-toe-board-game-icon-vector-illustration-design.jpg" alt="placeholder" className="card-preview__img" /></div>
                   <div className="card-content">
                     <h4 className="card-content__title">Tic Tac Game</h4>
                     <small>Built using React JS, CSS </small>
-                    <p className="card-content__text">A simulated development of a digitized tic tac game for entertainment. Play against the computer.
-                    This classic game contributes to children's developmental growth in numerous ways including their understanding of predictability and problem solving.</p>
+                    <p className="card-content__text">
+                    A classic game that contributes to children's developmental growth in numerous ways including their understanding of predictability and problem solving.</p>
                     <p><a href="https://sean-code.github.io/tic-tac/" target="_blank">
                       View Online
                       </a></p>
@@ -93,73 +92,50 @@ function Projects(){
                 </div>
                 <div className="card">
                   <div className="card-preview">
-                    <img src="https://picsum.photos/500/300" alt="placeholder" className="card-preview__img" /></div>
+                    <img src="https://img.freepik.com/premium-vector/real-estate-logo_74869-159.jpg?w=740" alt="placeholder" className="card-preview__img" /></div>
                   <div className="card-content">
                     <h4 className="card-content__title">Vallie's Estate</h4>
-                    <p className="card-content__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae eos, sed nam minus magnam, aspernatur ratione a laboriosam odio suscipit deleniti quisquam repellat aliquid qui maiores doloremque quos consectetur. Nisi.</p>
-                    <p><a href="" target="_blank">
+                    <small>Built using HTML, and CSS</small>
+                    <p className="card-content__text">A web application that displays the business operations of a real estate company by the name of Vallie's Estate.</p>
+                    <p><a href="https://sean-code.github.io/TryRealEstate/" target="_blank">
                       View Online
                       </a></p>
-                    <p><a href="" target="_blank">
+                    <p><a href="https://github.com/sean-code/TryRealEstate" target="_blank">
                       View Repo
                     </a></p>
                   </div>
                 </div>
                 <div className="card">
-                  <div className="card-preview"><img src="https://picsum.photos/500/300" alt="placeholder" className="card-preview__img" /></div>
+                  <div className="card-preview"><img src="https://i.ibb.co/sKBr6gh/Screenshot-from-2022-12-18-16-07-51.png" alt="placeholder" className="card-preview__img" /></div>
                   <div className="card-content">
-                    <h4 className="card-content__title">Project title</h4>
-                    <p className="card-content__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae eos, sed nam minus magnam, aspernatur ratione a laboriosam odio suscipit deleniti quisquam repellat aliquid qui maiores doloremque quos consectetur. Nisi.</p>
-                    <p><a href="" target="_blank">
+                    <h4 className="card-content__title">Car-Reviewing-Site</h4>
+                    <small>Built using React JS, , CSS, RubyOnRails(back-end)</small>
+                    <p className="card-content__text">An application where users can view car ratings,average market price, used car inventory, market car reviews, search for a car and add a new car in the system</p>
+                    <p><a href="https://sean-code.github.io/Client-Car-Review-App/" target="_blank">
                       View Online
                       </a></p>
-                    <p><a href="" target="_blank">
-                      View Repo
-                    </a></p>
-                  </div>
-                </div>
-              </div>
-              <div className="card">
-                  <div className="card-preview"><img src="https://picsum.photos/500/300" alt="placeholder" className="card-preview__img" /></div>
-                  <div className="card-content">
-                    <h4 className="card-content__title">Project title</h4>
-                    <p className="card-content__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae eos, sed nam minus magnam, aspernatur ratione a laboriosam odio suscipit deleniti quisquam repellat aliquid qui maiores doloremque quos consectetur. Nisi.</p>
-                    <p><a href="" target="_blank">
-                      View Online
-                      </a></p>
-                    <p><a href="" target="_blank">
+                    <p><a href="https://github.com/sean-code/Client-Car-Review-App" target="_blank">
                       View Repo
                     </a></p>
                   </div>
                 </div>
 
-              <div className="card">
-                  <div className="card-preview"><img src="https://picsum.photos/500/300" alt="placeholder" className="card-preview__img" /></div>
+                <div className="card">
+                  <div className="card-preview">
+                    <img src="https://github.com/sean-code/Superheroes-Back-end/raw/main/imgs/heroes.png" alt="placeholder" className="card-preview__img" /></div>
                   <div className="card-content">
-                    <h4 className="card-content__title">Project title</h4>
-                    <p className="card-content__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae eos, sed nam minus magnam, aspernatur ratione a laboriosam odio suscipit deleniti quisquam repellat aliquid qui maiores doloremque quos consectetur. Nisi.</p>
-                    <p><a href="" target="_blank">
+                    <h4 className="card-content__title">Superheroes API</h4>
+                    <small>Ruby on Rails, Postgres, Railway</small>
+                    <p className="card-content__text">An API comprising superheroes and their powers. The characters used are from the Amazon Prime's TV show entitled THE BOYS.</p>
+                    <p><a href="https://sean-code.github.io/tic-tac/" target="_blank">
                       View Online
                       </a></p>
-                    <p><a href="" target="_blank">
+                    <p><a href="https://github.com/sean-code/tic-tac/" target="_blank">
                       View Repo
-                    </a></p>
-                  </div>
-              </div>
-              <div className="card">
-                  <div className="card-preview"><img src="https://picsum.photos/500/300" alt="placeholder" className="card-preview__img" /></div>
-                  <div className="card-content">
-                    <h4 className="card-content__title">Project title</h4>
-                    <p className="card-content__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae eos, sed nam minus magnam, aspernatur ratione a laboriosam odio suscipit deleniti quisquam repellat aliquid qui maiores doloremque quos consectetur. Nisi.</p>
-                    <p><a href="" target="_blank">
-                      View Online
                       </a></p>
-                    <p><a href="" target="_blank">
-                      View Repo
-                    </a></p>
                   </div>
+                </div>
               </div>
-
             </section>
           </article>
         </main>
