b232fcd72d7c18d5417c0c9c61a98bf11f67e3e5
sean-code <john.nganga@student.moringaschool.com>
2023-03-25 00:24:22 +0300

    Update: Add education and change icons

--- src/components/education/education.js
+++ src/components/education/education.js
@@ -15,7 +15,7 @@ return (
             </Card.Text>
         </Card>
         <Card className='education-card'>
-            <Card.Img variant="top" src="https://media.istockphoto.com/id/1399470424/vector/luxury-certificate-of-appreciation-template-with-red-and-gold-color-multipurpose-certificate.jpg?s=612x612&w=0&k=20&c=dGHpqfFSiyQr1N0RTmf0gOL9Nn0tld-w4YqjnACuBgw=" />
+            <Card.Img variant="top" src="https://media.istockphoto.com/id/536058765/photo/mortar-board-and-piggy-bank.jpg?s=612x612&w=0&k=20&c=dExyC-Nq5dq5GPdrfYoqqq1VuaY8xjTSZu8xI6NdM0c=" />
             <Card.Body>
             <Card.Title>Diploma in Computer Science</Card.Title>
             </Card.Body>
@@ -32,13 +32,22 @@ return (
                 2023
             </Card.Text>
         </Card>
-        <Card>
-            <Card.Img variant="top" src="https://media.istockphoto.com/id/1399470424/vector/luxury-certificate-of-appreciation-template-with-red-and-gold-color-multipurpose-certificate.jpg?s=612x612&w=0&k=20&c=dGHpqfFSiyQr1N0RTmf0gOL9Nn0tld-w4YqjnACuBgw=" />
+        <Card className='education-card'>
+            <Card.Img variant="top" src="https://media.istockphoto.com/id/536058765/photo/mortar-board-and-piggy-bank.jpg?s=612x612&w=0&k=20&c=dExyC-Nq5dq5GPdrfYoqqq1VuaY8xjTSZu8xI6NdM0c=" />
             <Card.Body>
             <Card.Title>Certificate in Network Fundamentals</Card.Title>
             </Card.Body>
             <Card.Text>
                 2021
+            </Card.Text>
+        </Card>
+        <Card>
+            <Card.Img variant="top" src="https://media.istockphoto.com/id/891698738/photo/mortar-and-degree.jpg?s=612x612&w=0&k=20&c=OBPBQicp4iU7HmCRBEr8SVcAB3iRF79xF0dh5y3L4DY=" />
+            <Card.Body>
+            <Card.Title>Certificate in Web Design</Card.Title>
+            </Card.Body>
+            <Card.Text>
+                2023
             </Card.Text>
         </Card>
     </CardGroup>
